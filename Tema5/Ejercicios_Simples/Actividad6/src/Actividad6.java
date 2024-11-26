4464dfd6ee99353f41b537bf6b5c6ca11af31b4e
lierniSR <lierniSR@github.com>
2024-11-26 17:40:42 +0100

    Actividad 6 terminado

---
+++ Tema5/Ejercicios_Simples/Actividad6/src/Actividad6.java
@@ -0,0 +1,10 @@
+import Dao.DaoCasa;
+
+public class Actividad6 {
+    public static void main(String[] args) {
+        //DaoCasa.insertarDatos();
+        DaoCasa.listarCasaSevilla();
+        DaoCasa.actualizarNombreCiudad();
+        DaoCasa.listarCasaSevilla();
+    }
+}
