9e16003b48eaf4512c15067685e675d7d1c160d6
lierniSR <lierniSR@github.com>
2024-11-26 16:49:43 +0100

    Actividad 3 terminado

---
+++ Tema5/Ejercicios_Simples/Actividad3/src/Actividad3.java
@@ -0,0 +1,8 @@
+import Dao.DaoCasa;
+
+public class Actividad3 {
+    public static void main(String[] args) {
+        DaoCasa.insertarDatos();
+        DaoCasa.listarHuesped30();
+    }
+}
---
+++ Tema5/Ejercicios_Simples/Actividad3/src/Conexion/ConexionDB4O.java
@@ -0,0 +1,23 @@
+package Conexion;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+public class ConexionDB4O {
+
+    private static ObjectContainer db = null;
+
+    public static ObjectContainer conectar() {
+        if (db == null) {
+            db = Db4oEmbedded.openFile("Actividad3.db4o");
+        }
+        return db;
+    }
+
+    public static void desconectar() {
+        if (db != null) {
+            db.close();
+            db = null;
+        }
+    }
+}
