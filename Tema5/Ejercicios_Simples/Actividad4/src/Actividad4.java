388616b76c458c67f5ea71fcf1e05e9800cfc60a
lierniSR <lierniSR@github.com>
2024-11-26 17:00:49 +0100

    Actividad 4 terminado

---
+++ Tema5/Ejercicios_Simples/Actividad4/src/Actividad4.java
@@ -0,0 +1,10 @@
+import Dao.DaoCasa;
+
+public class Actividad4 {
+    public static void main(String[] args) {
+        //DaoCasa.insertarDatos();
+        DaoCasa.listarCasas();
+        DaoCasa.actualizarHabitaciones();
+        DaoCasa.listarCasas();
+    }
+}
